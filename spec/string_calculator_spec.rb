d6a299f5e139ec097400f19d040d4e18381d0d79
bv-anubhav <anubhav@blogvault.net>
2025-04-22 23:09:07 +0530

    basic operations with "", "1", "1,2"

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -5,5 +5,15 @@ RSpec.describe StringCalculator do
     calc = StringCalculator.new
     expect(calc.add("")).to eq(0)
   end
+
+	it "return num when there is only 1 num" do
+		calc = StringCalculator.new
+    expect(calc.add("1")).to eq(1)
+	end
+
+	it "returns the sum of two numbers separated by a comma" do
+    calc = StringCalculator.new
+    expect(calc.add("1,2")).to eq(3)
+  end
 end
 
--- string_calculator.rb
+++ string_calculator.rb
@@ -1,2 +1,7 @@
 class StringCalculator
+	def add(string_num)
+		return 0 if string_num.empty?
+		return string_num.to_i if string_num.split(",").size == 1
+		return string_num.split(",").map(&:to_i).sum
+	end
 end
